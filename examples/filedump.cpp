7e1dcca4fbf525dcd4aa40ab01bfdaea57be7b68
Andrey Borovsky <sobaka@andreyb2.net>
2023-05-02 21:11:34 +0200

    example added

---
+++ examples/filedump.cpp
@@ -0,0 +1,22 @@
+#include "directdltfile.h"
+#include "transferredfiles.h"
+#include <stdio.h>
+
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    if (argc == 1) {
+        printf("This program dumps files attached to the given dlt file via file transfer.\n");
+        return 0;
+    }
+    DLTReader::DirectDLTFile f(argv[1]);
+    DLTReader::TransferredFiles files(f.begin(), f.end());
+    while (files.findFile()) {
+        printf("%s %i %s\n", files.currentFileName().data(), files.currentFileSize(), files.currentFileDate().data());
+        auto contents = files.getCurrentFileContents();
+        printf("%s\n", contents.data());
+    }
+    return 0;
+}
